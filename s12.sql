db87c6aa900b0269964ab2e6d3d6933174b9de02
Ezra <125749134+ezra236@users.noreply.github.com>
2025-01-27 23:27:05 -0800

    Create s12.sql

---
+++ s12.sql
@@ -0,0 +1,12 @@
+
+
+USE website_db;
+
+CREATE TABLE `right-shampoo` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `image_path` VARCHAR(255) NOT NULL
+);
+
+INSERT INTO `right-shampoo` (`image_path`) 
+VALUES ('h1.jpg');
+
